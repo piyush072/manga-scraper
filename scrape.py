0c7319ae5fb7172279b5b8f6e4f614b22cc633b9
Piyush <2017ugcs007@nitjsr.ac.in>
2019-06-08 22:12:45 +0530

    fixed errors

--- scrape.py
+++ scrape.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import time
 import urllib
 import os
+from pyvirtualdisplay import Display
 
 def ProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█'):
 
@@ -15,73 +16,93 @@ def ProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, lengt
     if iteration == total:
         print()
 
+def scrape():
+    display = Display(visible=0, size=(800, 600))
+    display.start()
 
-browser = webdriver.Chrome("/home/piyush/Downloads/chromedriver_linux64/chromedriver")
-URL = "https://kissmanga.com"
-browser.get(URL+"/Manga/One-Piece")
+    browser = webdriver.Chrome("/home/piyush/Downloads/chromedriver_linux64/chromedriver")
 
-time.sleep(12)
+    URL = "https://kissmanga.com"
+    browser.get(URL)
+    print("Fetching Data .", end =" ")
+    time.sleep(10)
 
-search = browser.find_element_by_xpath("//input[@id='keyword']")
-search.send_keys("One Piece")
+    print()
 
-browser.find_element_by_xpath("//input[@id='imgSearch']").click()
-page = browser.page_source
-soup = BeautifulSoup(page, features='lxml')
+    search = browser.find_element_by_xpath("//input[@id='keyword']")
+    srch = input("Search: ")
+    search.send_keys(srch)
 
-link=[]
-name=[]
+    print("Searching")
 
-for t in soup.find_all('table',attrs={'class':'listing'}):
-    for a in t.find_all('a',href=True):
-        link.append(a['href'])
-        name.append(a.text.replace('\n','').strip())
+    browser.find_element_by_xpath("//input[@id='imgSearch']").click()
 
-print("Search Result")
-
-for i in range(0,len(link)):
-    print(i+1,name[i])
-
-x = int(input())
+    page = browser.page_source
+    soup = BeautifulSoup(page, features='lxml')
 
 
-browser.get(URL+link[x-1])
+    link=[]
+    name=[]
+
+    for t in soup.find_all('table',attrs={'class':'listing'}):
+        for a in t.find_all('a',href=True):
+            link.append(a['href'])
+            name.append(a.text.replace('\n','').strip())
+
+    print("Search Result:")
+
+    for i in range(0,len(link)):
+        print(str(i+1)+".",name[i])
+
+    x = int(input("Enter choice: "))
+
+    browser.get(URL+link[x-1])
 
 
-page = browser.page_source
-soup = BeautifulSoup(page, features='lxml')
+    page = browser.page_source
+    soup = BeautifulSoup(page, features='lxml')
 
-link=[]
-name=[]
+    link=[]
+    name=[]
 
-for t in soup.find_all('table',attrs={'class':'listing'}):
-    for a in t.find_all('a',href=True):
-        link.append(a['href'])
-        name.append(a.text.replace('\n','').strip())
+    for t in soup.find_all('table',attrs={'class':'listing'}):
+        for a in t.find_all('a',href=True):
+            link.append(a['href'])
+            name.append(a.text.replace('\n','').strip())
 
-browser.get(URL+link[0])
-chp = name[0]
-my_path = '/home/piyush/Downloads/'+chp
-os.mkdir(my_path)
-page = browser.page_source
-soup = BeautifulSoup(page, features='lxml')
+    for i in range(0,11):
+        print(str(i+1)+".",name[i])
 
-image = []
+    x = int(input("Enter choice: "))
 
-for div in soup.find_all('div',attrs={'id':'divImage'}):
-    for img in div.find_all('img'):
-        image.append(img['src'])
+    browser.get(URL+link[x-1])
+    chp = name[x-1]
 
-l=len(image)
+    my_path = '/home/piyush/Downloads/'+chp
+    os.mkdir(my_path)
+    page = browser.page_source
+    soup = BeautifulSoup(page, features='lxml')
 
-ProgressBar(0, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
+    image = []
 
-for i in range(0,l):
-    na = str(i)+"."+image[i].split('.')[len(image[i].split('.'))-1]
+    for div in soup.find_all('div',attrs={'id':'divImage'}):
+        for img in div.find_all('img'):
+            image.append(img['src'])
 
-    urllib.request.urlretrieve(image[i], os.path.join(my_path, na))
+    l=len(image)
 
-    ProgressBar( i, l, prefix = 'Progress', suffix = 'Complete', length = 50)
+    ProgressBar(0, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
+
+    for i in range(0,l):
+        na = str(i)+"."+image[i].split('.')[len(image[i].split('.'))-1]
+
+        urllib.request.urlretrieve(image[i], os.path.join(my_path, na))
+
+        ProgressBar( i, l, prefix = 'Progress', suffix = 'Complete', length = 50)
 
 
-browser.close()
+    browser.close()
+    display.stop()
+
+if __name__=="__main__":
+    scrape()
