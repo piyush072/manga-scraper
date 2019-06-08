625fd3b0d12c44830b9273d06a58283bb64ff864
Piyush <2017ugcs007@nitjsr.ac.in>
2019-06-08 19:57:15 +0530

    added search for manga

--- scrape.py
+++ scrape.py
@@ -6,6 +6,35 @@ browser = webdriver.Chrome("/home/piyush/Downloads/chromedriver_linux64/chromedr
 URL = "https://kissmanga.com"
 browser.get(URL)
 
-time.sleep(7)
+time.sleep(12)
 
-browser.find_element_by_xpath("//a[@id='liMostViewtab']").click()
+search = browser.find_element_by_xpath("//input[@id='keyword']")
+search.send_keys("One Piece")
+
+browser.find_element_by_xpath("//input[@id='imgSearch']").click()
+page = browser.page_source
+soup = BeautifulSoup(page, features='lxml')
+
+browser.close()
+
+link=[]
+name=[]
+
+for t in soup.find_all('table',attrs={'class':'listing'}):
+    for a in t.find_all('a',href=True):
+        link.append(a['href'])
+        name.append(a.text.replace('\n','').strip())
+
+print("Search Result")
+
+for i in range(0,len(link)):
+    print(i+1,name[i])
+
+x = int(input())
+
+URL = "https://kissmanga.com"+link[x-1]
+
+browser = webdriver.Chrome("/home/piyush/Downloads/chromedriver_linux64/chromedriver")
+browser.get(URL)
+
+time.sleep(12)
